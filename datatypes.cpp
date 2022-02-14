1807567f3be01fe105f489d2957024d9291f3508
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-14 12:06:49 -0600

    edits datatypes

--- datatypes.cpp
+++ datatypes.cpp
@@ -5,7 +5,6 @@ int main()
 {
   cout << "Size of char : " << sizeof(char) << endl;
   cout << "Size of int : " << sizeof(int) << endl;
-  ;
   cout << "Size of short int : " << sizeof(short int) << endl;
   cout << "Size of long int : " << sizeof(long int) << endl;
   cout << "Size of float : " << sizeof(float) << endl;
