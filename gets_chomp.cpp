bc682924735b8595992617948d8dcc9d126f9479
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-14 12:39:18 -0600

    edits to perform getline() function

--- gets_chomp.cpp
+++ gets_chomp.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-  int x;
-  cout << "Type a number: \n";
-  cin >> x;
-  cout << "Your number is: " << x << endl;
+  // int x;
+  // cout << "Type a number: \n";
+  // cin >> x;
+  // cout << "Your number is: " << x << endl;
+  string fullName;
+  cout << "Type your full name: \n";
+  getline(cin, fullName);
+  cout << "Your name is: " << fullName << endl;
   return 0;
 }
