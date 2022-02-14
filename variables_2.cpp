2139a405343b5df220307eab557d1b64e2c70f95
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-14 12:13:12 -0600

    intializes multiple variables

--- variables_2.cpp
+++ variables_2.cpp
@@ -10,8 +10,7 @@ int main()
   bool myBoolean = true;
   int myAge = 35;
   cout << "I am " << myAge << " years old. \n";
-  int x = 5;
-  int y = 6;
+  int x = 5, y = 6, z = 50;
   int sum = x + y;
   cout << sum << endl;
   return 0;
