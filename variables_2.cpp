02e7de61c4a220341d1e8cb3d23312c1bd6823f7
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-14 12:12:14 -0600

    intializes more variables

--- variables_2.cpp
+++ variables_2.cpp
@@ -8,4 +8,11 @@ int main()
   char myLetter = 'D';
   string myText = "Hello";
   bool myBoolean = true;
+  int myAge = 35;
+  cout << "I am " << myAge << " years old. \n";
+  int x = 5;
+  int y = 6;
+  int sum = x + y;
+  cout << sum << endl;
+  return 0;
 }
