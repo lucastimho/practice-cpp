2d6da090b35017a0c1fffc343eb27c33ba034033
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-17 23:31:57 -0600

    adds question from the matrix

---
+++ matrix_pill.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  cout << "Welcome to the Matrix. Do you want to take the red pill or blue pill? \n";
+  string pill;
+  cin >> pill;
+  if (pill == "red")
+  {
+    cout << "You  took the red pill. \n";
+  }
+  else if (pill == "blue")
+  {
+    cout << "You took the blue pill. \n";
+  }
+  return 0;
+}
