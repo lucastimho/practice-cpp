614195083760b5978f908f95a734d4ccf24e78ef
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-17 22:15:41 -0600

    adds variable checkpoint

---
+++ variables_check.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int Robert = 23;
+  int Lily = 18;
+  int Lori = 19;
+  int Alvin = 32;
+  float average = (Robert + Lily + Lori + Alvin) / 4;
+  cout << average << endl;
+
+  int x = 3;
+  int y = 9;
+  cout << x + y << endl;
+  return 0;
+}
